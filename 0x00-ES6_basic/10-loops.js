00974e8f9ebeea89f59081bdaceff08acff6fc4b
christopher Dominic <chriseze0@.com>
2024-07-22 22:46:57 +0000

    first commit

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const val of array) {
+    newArray.push(appendString + val);
+  }
+
+  return newArray;
+}
+
